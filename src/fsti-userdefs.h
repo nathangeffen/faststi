80140867cbb67d67ad035320cedc5fe0c396cf73
Nathan Geffen <nathangeffen@gmail.com>
2018-10-11 23:46:52 +0200

    Stefan's syphilis project.

--- src/fsti-userdefs.h
+++ src/fsti-userdefs.h
@@ -8,4 +8,52 @@
 #ifndef FSTI_USERDEFS // Don't remove these guards
 #define FSTI_USERDEFS
 
+#define FSTI_AGENT_FIELDS                                       \
+    double re_sexact;                                           \
+    double relstat;                                             \
+    double reldur;                                              \
+    bool hiv;
+
+#define FSTI_HOOK_AFTER_MATCH(simulation, a, b) hestia(simulation, a, b)
+
+#define FSTI_AGENT_PRINT_CSV(file_handle, id, agent)                  \
+    fprintf(file_handle, "%u,%zu,%u,%u,%u,%u,%u,%u,%f,%f,%ld\n",     \
+            id,                                                       \
+            agent->id,                                                \
+            (unsigned) agent->sex,                                    \
+            (unsigned) agent->sex_preferred,                          \
+            (unsigned) agent->birthday,                               \
+            (unsigned) agent->age_group,                              \
+            agent->infected,                                          \
+            agent->hiv,                                               \
+            agent->re_sexact,                                         \
+            agent->relstat,                                           \
+            agent->num_partners ? (long) agent->partners[0] : -1)
+
+#define FSTI_CSV_ENTRIES                                        \
+    FSTI_CSV_ENTRY(id, fsti_to_size_t)                          \
+    FSTI_CSV_ENTRY(birthday, fsti_to_uint16_t)                   \
+    FSTI_CSV_ENTRY(age_group, fsti_to_uint16_t)                 \
+    FSTI_CSV_ENTRY(sex, fsti_to_uchar)                          \
+    FSTI_CSV_ENTRY(sex_preferred, fsti_to_uchar)                \
+    FSTI_CSV_ENTRY(re_sexact, fsti_to_double)                   \
+    FSTI_CSV_ENTRY(relstat, fsti_to_double)                     \
+    FSTI_CSV_ENTRY(hiv, fsti_to_bool)                           \
+    FSTI_CSV_ENTRY(partners[0], fsti_to_partner)
+
+#define FSTI_HOOK_EVENTS_REGISTER                       \
+    fsti_register_add("AGE", event_age);                \
+    fsti_register_add("HADES", event_hades);            \
+    fsti_register_add("EROS", event_eros);              \
+    fsti_register_add("DIONYSUS", event_dionysus);      \
+    fsti_register_add("APHRODITE", event_aphrodite);
+
+// Prototypes
+void event_age(struct fsti_simulation *simulation);
+void event_hades(struct fsti_simulation *simulation);
+void event_eros(struct fsti_simulation *simulation);
+void event_dionysus(struct fsti_simulation *simulation);
+void event_aphrodite(struct fsti_simulation *simulation);
+void hestia(struct fsti_simulation *simulation,
+            struct fsti_agent *a, struct fsti_agent *b);
 #endif
